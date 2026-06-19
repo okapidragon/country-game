c0e9aaa97d3d5829906c4590e2662bd9e37f236c
okapidragon <142844243+okapidragon@users.noreply.github.com>
2026-06-19 10:02:03 -0400

    Fix logic to get independent countries from API

--- api/index.py
+++ api/index.py
@@ -9,10 +9,11 @@ app = Flask(__name__, template_folder=template_dir)
 
 def get_random_independent_country(level):
     try:
-            response = requests.get('https://restcountries.com/v5/all?response_fields=names.common,classification.sovereign,demographics.population,geography.continents,capitals,flag.emoji&limit=250',
+        response = requests.get(
+            'https://restcountries.com/v5/all?response_fields=names.common,classification.sovereign,demographics.population,geography.continents,capitals,flag.emoji&limit=250',
             headers={'Authorization': 'Bearer rc_live_9ad44e25448b435e9999a2dffc64817f'})
         all_countries = response.json().get('data', [])
-        independent_list = [c for c in all_countries if c.get("independent")]
+        independent_list = [c for c in all_countries if c.get("classification", {}).get("sovereign")]
         
         if not independent_list:
             return None
